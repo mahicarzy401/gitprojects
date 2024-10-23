6b7f1a499c5545ecd68bc310297ff5c6daed09ac
mahirah <mahicarzy401@gmail.com>
2024-10-23 17:04:08 +0500

    new commit

--- src/PostgresSQLJDBC.java
+++ src/PostgresSQLJDBC.java
@@ -3,12 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class PostgresSQLJDBC {
+    //new comment added 
    public static void main(String[] args) {
       String url = "jdbc:postgresql://localhost:5432/postgres";
       String user = "postgres";
       String password = "root";
       
-      try (Connection conn = DriverManager.getConnection(url, user, password)) {
+      try (Connection conn1 = DriverManager.getConnection(url, user, password)) {
           System.out.println("Connected to the PostgreSQL server successfully.");
       } catch (SQLException e) {
           System.out.println("Connection failure!");
