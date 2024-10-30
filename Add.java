06103fabbd31be158f33e614f64c82a5d6b8b74f
mahirah <mahicarzy401@gmail.com>
2024-10-30 13:08:41 +0500

    new commit

---
+++ Add.java
@@ -0,0 +1,12 @@
+class Add{
+    public static void main(String [] args ){
+        int x= 5;
+        int y = 25;
+        if (x>y){
+        System.out.println("YES");
+        }
+        else {
+           System.out.println("NO");
+      }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main 
+    {
+       int x = 5;
+    }
+class Second {
+      public static void main(String[] args)
+           {
+         Main myObj = new Main ();
+         System.out.println(myObj.x);
+             }
+               }
